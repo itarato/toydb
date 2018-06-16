b49f23a6c9f1b1a0442b346e4447724c590cd011
Peter Arato <it.arato@gmail.com>
2018-06-16 11:57:00 -0400

    Make server standalone.

---
+++ src/dbserver.rs
@@ -0,0 +1,71 @@
+use engine_operator;
+use futures::{future, Future, Stream};
+use hyper::rt;
+use hyper::service::service_fn;
+use hyper::{self, Body, Method, Request, Response, Server, StatusCode};
+use query_parser;
+use std::str;
+use std::sync::{Arc, Mutex};
+
+#[derive(Debug, Default)]
+pub struct DBServer {
+  engine_operator: Arc<Mutex<engine_operator::EngineOperator>>,
+  query_parser: Arc<query_parser::QueryParser>,
+}
+
+impl DBServer {
+  pub fn new() -> DBServer {
+    Default::default()
+  }
+
+  pub fn run(&self) {
+    let addr = ([127, 0, 0, 1], 8421).into();
+    let eo = self.engine_operator.clone();
+    let qp = self.query_parser.clone();
+    rt::run(rt::lazy(move || {
+      let eo = eo.clone();
+      let qp = qp.clone();
+      let new_service = move || {
+        let eo = eo.clone();
+        let qp = qp.clone();
+        service_fn(move |req| prepare_response(req, eo.clone(), qp.clone()))
+      };
+      let server = Server::bind(&addr).serve(new_service).map_err(|_| ());
+      server
+    }));
+  }
+}
+
+fn prepare_response(
+  req: Request<Body>,
+  engine_operator: Arc<Mutex<engine_operator::EngineOperator>>,
+  query_parser: Arc<query_parser::QueryParser>,
+) -> Box<Future<Item = Response<Body>, Error = hyper::Error> + Send> {
+  match req.method() {
+    &Method::POST => {
+      let fut = req.into_body().concat2().and_then(move |chunk| {
+        let value = str::from_utf8(chunk.as_ref()).unwrap().to_owned();
+
+        match query_parser.parse(&value) {
+          Ok(query) => {
+            let mut engine_operator = engine_operator.lock().unwrap();
+            engine_operator.execute(query);
+          }
+          Err(_) => {}
+        };
+
+        future::ok(Response::new(Body::empty()))
+      });
+      Box::new(fut)
+    }
+    _ => {
+      let body = Body::empty();
+      Box::new(future::ok(
+        Response::builder()
+          .status(StatusCode::NOT_FOUND)
+          .body(body)
+          .unwrap(),
+      ))
+    }
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+extern crate futures;
+extern crate hyper;
 
+mod dbserver;
 mod engine;
 mod engine_operator;
 mod query;
@@ -9,19 +12,19 @@ mod query_parser;
 mod repl;
 mod util;
 
-use std::env;
-
 fn main() {
     env_logger::init();
 
     info!("DB is starting");
 
-    let repl = repl::Repl::new();
+    // let repl = repl::Repl::new();
 
-    if let Some(file_name) = env::args().nth(1) {
-        info!("Got file-argument: {}", file_name);
-        let _ = repl.read_file(&file_name);
-    }
+    // if let Some(file_name) = env::args().nth(1) {
+    //     info!("Got file-argument: {}", file_name);
+    //     let _ = repl.read_file(&file_name);
+    // }
 
-    repl.start();
+    // repl.start();
+
+    dbserver::DBServer::new().run();
 }
